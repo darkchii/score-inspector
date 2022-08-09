29bc626ea4cccf0ade0ed8e220e9b1b43d2eb757
Amayakase <engineermarkpro@gmail.com>
2022-08-09 17:07:56 +0200

    Added stepper to force user to read what to do

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { CssBaseline, Box, AppBar, Toolbar, Typography, Button, Paper, Grid, CircularProgress, Tabs, Tab, Snackbar, Alert, Tooltip, Link } from '@mui/material';
+import { CssBaseline, Box, AppBar, Toolbar, Typography, Button, Paper, Grid, CircularProgress, Tabs, Tab, Snackbar, Alert, Tooltip, Link, Stepper, Step, StepLabel, CardContent, Card } from '@mui/material';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import React, { useEffect } from 'react';
 import './App.css';
@@ -113,6 +113,21 @@ function App() {
     });
   }
 
+  const [stepperActiveStep, setStepperActiveStep] = React.useState(0);
+  const [stepperCompleted, setStepperCompleted] = React.useState({});
+
+  const stepperHandleBack = () => {
+    setStepperActiveStep((prevActiveStep) => prevActiveStep - 1);
+  };
+
+  const stepperHandleNext = () => {
+    setStepperActiveStep((prevActiveStep) => prevActiveStep + 1);
+  };
+
+  const stepperHandleReset = () => {
+    setStepperActiveStep(0);
+  };
+
   return (
     <ThemeProvider theme={darkTheme}>
       <CssBaseline />
@@ -160,16 +175,58 @@ function App() {
               justifyContent="center"
               sx={{ py: 2, backgroundImage: `url("${(user !== undefined && user !== null ? user.cover_url : "null")}")` }}
             >
-              <Typography>To get your scores, head over to the osu!alternative Discord server, and follow the guide to fetching scores.<br />
+              <Card sx={{ width: '60%' }}>
+                <CardContent>
+                  <Box>
+                    <Stepper activeStep={stepperActiveStep}>
+                      <Step key="s1">
+                        <StepLabel>Join Discord</StepLabel>
+                      </Step>
+                      <Step key="s2">
+                        <StepLabel>Fetch Scores</StepLabel>
+                      </Step>
+                      <Step key="s3">
+                        <StepLabel>Download Scores</StepLabel>
+                      </Step>
+                      <Step key="s4">
+                        <StepLabel>Upload</StepLabel>
+                      </Step>
+                    </Stepper>
+                    <Typography sx={{ mt: 2, mb: 1 }}>Step {stepperActiveStep + 1}</Typography>
+                    <Box sx={{ px: 5 }} alignItems="center" justifyContent="center" direction="column">
+                      {
+                        {
+                          0: <><Typography sx={{ mt: 2, mb: 1 }}>Join the following discord: <Button href='https://discord.gg/VZWRZZXcW4' target="_blank" variant="contained" size='small'>osu!alternative</Button></Typography></>,
+                          1: <><Typography sx={{ mt: 2, mb: 1 }}>Go to the #info text-channel, and do what the section "Fetching scores" says.<br />It can take a couple of hours to get all of your scores ready, depending on your stats.</Typography></>,
+                          2: <><Typography sx={{ mt: 2, mb: 1 }}>To get your file, go to #bot-channel and perform the following command: <br /><code>!getfile -type scores -u username</code> - Replace username with yours. You will receive a <code>scores.csv</code> file.</Typography></>,
+                          3: <><Typography sx={{ mt: 2, mb: 1 }}>Upload the downloaded <code>scores.csv</code> file here:<br /><Button variant="contained" color="success" component="label" disabled={loadState}>Upload scores<input onChange={e => handleScoresUpload(e.target.files[0])} hidden accept=".csv" type="file" /></Button></Typography></>,
+                        }[stepperActiveStep]
+                      }
+                    </Box>
+                    <Box sx={{ display: 'flex', flexDirection: 'row', pt: 2 }}>
+                      <Button
+                        color="inherit"
+                        disabled={stepperActiveStep === 0}
+                        onClick={stepperHandleBack}
+                        sx={{ mr: 1 }}
+                      >
+                        Back
+                      </Button>
+                      <Box sx={{ flex: '1 1 auto' }} />
+                      {
+                        stepperActiveStep < 3 ?
+                          <Button onClick={stepperHandleNext}>Next</Button>
+                          : <></>
+                      }
+                    </Box>
+                  </Box>
+                </CardContent>
+              </Card>
+              {/* <Typography>To get your scores, head over to the osu!alternative Discord server, and follow the guide to fetching scores.<br />
                 When the fetcher is complete, run <code>!getfile -type scores</code> in the bot channel. Upload the resulting file here.<br /><br />
-                <Button href='https://discord.gg/VZWRZZXcW4' target="_blank" variant="contained" size='small'>
-                  Join osu!alternative
-                </Button>
+                <Button href='https://discord.gg/VZWRZZXcW4' target="_blank" variant="contained" size='small'>Join osu!alternative</Button>
               </Typography><br /><br />
-              <Button variant="contained" color="success" component="label" disabled={loadState}>
-                Upload scores
-                <input onChange={e => handleScoresUpload(e.target.files[0])} hidden accept=".csv" type="file" />
-              </Button>
+              <Button variant="contained" color="success" component="label" disabled={loadState}>Upload scores<input onChange={e => handleScoresUpload(e.target.files[0])} hidden accept=".csv" type="file" /></Button> */}
             </Grid>
             {loadState ? <>
               <br />
