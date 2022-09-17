0e0fae585585bb44821d1ae895b57c033741dd71
Amayakase <engineermarkpro@gmail.com>
2022-09-17 22:38:28 +0200

    Set update date

--- src/updates.js
+++ src/updates.js
@@ -8,72 +8,42 @@ const CHANGETYPES = {
 const updates = [
     {
         version: '1.0.2',
-        date: 'n/a',
+        date: '2022-09-17',
         changes: [
-            [CHANGETYPES.NEW, 'Added 2016 performance calculator and topplays for it', '06004fa8a6e36633da4f6d8146544515d5a1a1fa'
-            ],
-            [CHANGETYPES.NEW, 'Added pp difference on \'if\' cards on general', '4e3f7fa1f05aa92686cbed3c8c7577a72ed9b191'
-            ],
-            [CHANGETYPES.NEW, 'New landing page', '7c6bc20ee434f553c9c111f3758007e6f5c1cb9d'
-            ],
-            [CHANGETYPES.NEW, 'Beatmap pack completion added', 'e3778c7a9459ec5e8b347514075b5008afbd6669'
-            ],
-            [CHANGETYPES.FIX, 'Fixed wrong combo on 100% accuracy pp calc on scoreview', 'a9cfb42967c46813f30b53e5f2b6524733eb4645'
-            ],
-            [CHANGETYPES.FIX, 'Fixed some scores showing outside any activity area', '1b11696749deec3fa26a4f9637fea54f87857204'
-            ],
-            [CHANGETYPES.FIX, 'Fixed completion cards missing the highest value (AR10, OD10, CS10)', '830aadb7a69cc9c0de1a6a5f2ef480753e5aad53'
-            ],
-            [CHANGETYPES.FIX, 'Fixed completion cards missing loved data', '830aadb7a69cc9c0de1a6a5f2ef480753e5aad53'
-            ],
-            [CHANGETYPES.MISC, 'Design overhaul', '4740f58373862be6fe41cca6adae275c143270f0'
-            ],
-            [CHANGETYPES.MISC, 'Dependencies version bumping', 'c9fd4dedfebcd430d6f584859c52c3b96128d5ac'
-            ],
+            [CHANGETYPES.NEW, 'Added 2016 performance calculator and topplays for it', '06004fa8a6e36633da4f6d8146544515d5a1a1fa'],
+            [CHANGETYPES.NEW, 'Added pp difference on \'if\' cards on general', '4e3f7fa1f05aa92686cbed3c8c7577a72ed9b191'],
+            [CHANGETYPES.NEW, 'New landing page', '7c6bc20ee434f553c9c111f3758007e6f5c1cb9d'],
+            [CHANGETYPES.NEW, 'Beatmap pack completion added', 'e3778c7a9459ec5e8b347514075b5008afbd6669'],
+            [CHANGETYPES.FIX, 'Fixed wrong combo on 100% accuracy pp calc on scoreview', 'a9cfb42967c46813f30b53e5f2b6524733eb4645'],
+            [CHANGETYPES.FIX, 'Fixed some scores showing outside any activity area', '1b11696749deec3fa26a4f9637fea54f87857204'],
+            [CHANGETYPES.FIX, 'Fixed completion cards missing the highest value (AR10, OD10, CS10)', '830aadb7a69cc9c0de1a6a5f2ef480753e5aad53'],
+            [CHANGETYPES.FIX, 'Fixed completion cards missing loved data', '830aadb7a69cc9c0de1a6a5f2ef480753e5aad53'],
+            [CHANGETYPES.MISC, 'Design overhaul', '4740f58373862be6fe41cca6adae275c143270f0'],
+            [CHANGETYPES.MISC, 'Dependencies version bumping', 'c9fd4dedfebcd430d6f584859c52c3b96128d5ac'],
         ]
     },
     {
         version: "1.0.1",
         date: "2022-09-10",
         changes: [
-            [CHANGETYPES.NEW,
-                "Added changelog page", '608894cd6bd388054761191fc128982340f1b2cb'
-            ],
-            [CHANGETYPES.NEW,
-                "Loved maps are now supported", 'f6317ec5f9314172fa75ddc98d8fbad2119a05e0'
-            ],
-            [CHANGETYPES.NEW,
-                "Score filtering", 'bab623f6cecf22993aba5b1558ace829390f45d2'
-            ],
-            [CHANGETYPES.NEW,
-                "Added xexxar bonus pp proposal data", '3d4f61041b02dff1d98748544bffc6bd0834e348'
-            ],
-            [CHANGETYPES.FIX,
-                "Beatmap URL in score view", '08c36bab47561562ff082a62d27e2ed5a122b31e'
-            ],
-            [CHANGETYPES.FIX,
-                "Date graphs current date was missing", 'dca94923470b6ceb8d1c8df636b3013a3e7e9cb6'
-            ],
-            [CHANGETYPES.MISC,
-                "Design updates", '4fbbcdfa45611ff2cbb24f53edd8b22560d385b6'
-            ],
-            [CHANGETYPES.MISC,
-                "Changed font to 'comfortaa'", '4fbbcdfa45611ff2cbb24f53edd8b22560d385b6'
-            ],
-            [CHANGETYPES.MISC,
-                "Dependency updates", 'cfa8381de91f85e8c07caf47e6c3c3124eb536fb'
-            ],
+            [CHANGETYPES.NEW, "Added changelog page", '608894cd6bd388054761191fc128982340f1b2cb'],
+            [CHANGETYPES.NEW, "Loved maps are now supported", 'f6317ec5f9314172fa75ddc98d8fbad2119a05e0'],
+            [CHANGETYPES.NEW, "Score filtering", 'bab623f6cecf22993aba5b1558ace829390f45d2'],
+            [CHANGETYPES.NEW, "Added xexxar bonus pp proposal data", '3d4f61041b02dff1d98748544bffc6bd0834e348'],
+            [CHANGETYPES.FIX, "Beatmap URL in score view", '08c36bab47561562ff082a62d27e2ed5a122b31e'],
+            [CHANGETYPES.FIX, "Date graphs current date was missing", 'dca94923470b6ceb8d1c8df636b3013a3e7e9cb6'],
+            [CHANGETYPES.MISC, "Design updates", '4fbbcdfa45611ff2cbb24f53edd8b22560d385b6'],
+            [CHANGETYPES.MISC, "Changed font to 'comfortaa'", '4fbbcdfa45611ff2cbb24f53edd8b22560d385b6'],
+            [CHANGETYPES.MISC, "Dependency updates", 'cfa8381de91f85e8c07caf47e6c3c3124eb536fb'],
         ]
     },
     {
         version: "1.0.0",
         date: "2022-08-06",
         changes: [
-            [CHANGETYPES.MISC,
-                "Untracked changes"
-            ],
+            [CHANGETYPES.MISC, "Untracked changes"],
         ]
     }
 ];
 
-export {updates, CHANGETYPES};
+export { updates, CHANGETYPES };
