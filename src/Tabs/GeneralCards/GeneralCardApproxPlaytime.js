28178946d7a240be48322460b9306b272af4a31a
Amayakase <engineermarkpro@gmail.com>
2022-09-19 20:57:01 +0200

    Improved playtime approximation

--- src/Tabs/GeneralCards/GeneralCardApproxPlaytime.js
+++ src/Tabs/GeneralCards/GeneralCardApproxPlaytime.js
@@ -6,13 +6,16 @@ import { useState } from 'react';
 function GeneralCardApproxPlaytime(props) {
     const [playtime, setPlaytime] = useState(null);
 
-    useEffect(()=>{
-        if(props.data.processed.sessions!==undefined){
+    useEffect(() => {
+        if (props.data.processed.sessions !== undefined) {
             let totalPlaytime = 0;
-            props.data.processed.sessions.forEach((session)=>{
+            props.data.processed.sessions.forEach((session) => {
                 totalPlaytime += session.length;
             });
-            setPlaytime(Math.round(totalPlaytime));
+            let idleTime = props.data.user.statistics.play_time - props.data.processed.total_length;
+            let playcountTime = props.data.user.statistics.play_count * (props.data.processed.average_length * 0.5);
+
+            setPlaytime(Math.round((totalPlaytime + playcountTime + idleTime * (Math.log10(idleTime)*0.1)) * 0.5));
         }
     }, []);
 
@@ -22,7 +25,7 @@ function GeneralCardApproxPlaytime(props) {
                 <CardContent>
                     <Grid container spacing={3} sx={{ justifyContent: 'space-between' }}>
                         <Grid item>
-                            <Typography color="textPrimary" variant="h4">{(moment.duration(playtime??0, "seconds").asHours()).toFixed(1)} hours</Typography>
+                            <Typography color="textPrimary" variant="h4">{(moment.duration(playtime ?? 0, "seconds").asHours()).toFixed(1)} hours</Typography>
                             <Typography color="textSecondary">approximate playtime</Typography>
                         </Grid>
                     </Grid>
