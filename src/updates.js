34b6f3ae8b24ad39a217170ce183574c00b7476d
Amayakase <engineermarkpro@gmail.com>
2022-09-19 21:06:09 +0200

    Changed reactjs port to a non-default value

--- src/updates.js
+++ src/updates.js
@@ -10,9 +10,11 @@ const updates = [
         version: '1.0.3',
         date: '2022-09-17',
         changes: [
+            [CHANGETYPES.NEW, 'Added approximate real playtime calculation', '28178946d7a240be48322460b9306b272af4a31a'],
             [CHANGETYPES.FIX, 'Fixed incorrect attribute display values', '858e36bbb4ae19e99db3237b2e96f47eae7187c8'],
             [CHANGETYPES.FIX, 'Fixed incorrect sessions with lone scores', 'f7a4807824f578b61ac4613a55d5674e8c804e94'],
             [CHANGETYPES.FIX, 'Optimized some processing', '77d8c808f2f7afddabed619a38f9f9b3e863ee94'],
+            [CHANGETYPES.MISC, 'Dependencies version bumping'],
         ]
     },
     {
