c7dd8a8da415577bb1f7a75df75594b750c65e30
Amayakase <engineermarkpro@gmail.com>
2022-09-19 23:42:46 +0200

    Make score rank fit its card

--- src/Tabs/GeneralCards/GeneralCardApproxPlaytime.js
+++ src/Tabs/GeneralCards/GeneralCardApproxPlaytime.js
@@ -1,7 +1,10 @@
-import { Card, CardContent, Typography, Grid } from '@mui/material';
+import { Card, CardContent, Typography, Grid, Tooltip } from '@mui/material';
 import moment from 'moment';
 import { useEffect } from 'react';
 import { useState } from 'react';
+import InfoOutlinedIcon from '@mui/icons-material/InfoOutlined';
+
+const infoText = 'This value is based off your playcount, profile playtime, and session playtime. It is not 100% accurate, but it is a good estimate.';
 
 function GeneralCardApproxPlaytime(props) {
     const [playtime, setPlaytime] = useState(null);
@@ -15,7 +18,7 @@ function GeneralCardApproxPlaytime(props) {
             let idleTime = props.data.user.statistics.play_time - props.data.processed.total_length;
             let playcountTime = props.data.user.statistics.play_count * (props.data.processed.average_length * 0.5);
 
-            setPlaytime(Math.round((totalPlaytime + playcountTime + idleTime * (Math.log10(idleTime)*0.1)) * 0.5));
+            setPlaytime(Math.round((totalPlaytime + playcountTime + idleTime * (Math.log10(idleTime) * 0.1)) * 0.5));
         }
     }, []);
 
@@ -26,7 +29,7 @@ function GeneralCardApproxPlaytime(props) {
                     <Grid container spacing={3} sx={{ justifyContent: 'space-between' }}>
                         <Grid item>
                             <Typography color="textPrimary" variant="h4">{(moment.duration(playtime ?? 0, "seconds").asHours()).toFixed(1)} hours</Typography>
-                            <Typography color="textSecondary">approximate playtime</Typography>
+                            <Typography sx={{ display: 'flex' }} color="textSecondary">approximate playtime <Tooltip title={infoText}><InfoOutlinedIcon sx={{ ml: 1 }} /></Tooltip></Typography>
                         </Grid>
                     </Grid>
                 </CardContent>
--- src/Tabs/GeneralCards/GeneralCardScoreRank.js
+++ src/Tabs/GeneralCards/GeneralCardScoreRank.js
@@ -1,16 +1,22 @@
 import { Card, CardContent, Typography, Grid } from '@mui/material';
+import { useEffect, useState } from 'react';
 import NumberFormat from 'react-number-format';
 
 function GeneralCardScoreRank(props) {
+    const [rank, setRank] = useState(0);
+
+    useEffect(() => {
+        setRank(props.data.user.scoreRank);
+    }, []);
     return (
         <>
-            <Card>
+            <Card sx={{height:'100%'}}>
                 <CardContent>
                     <Grid container spacing={3} sx={{ justifyContent: 'space-between' }}>
                         <Grid item>
-                            <Typography color="textPrimary" variant="h4">
-                                {props.data.user.scoreRank !== undefined && props.data.user.scoreRank > 0 ?
-                                    <>#<NumberFormat displayType={'text'} thousandSeparator={true} value={props.data.user.scoreRank.toLocaleString("en-US")} /></>
+                            <Typography color="textPrimary" variant={rank < 10000 ? 'h4' : 'h5'}>
+                                {rank <= 10000 ?
+                                    <>#{props.data.user.scoreRank.toLocaleString("en-US")}</>
                                     : <>&gt;#10,000</>}
                             </Typography>
                             <Typography color="textSecondary">score rank</Typography>
