1730f1b46ecd74ef7606825b4a8110feca10e91d
Amayakase <engineermarkpro@gmail.com>
2022-10-07 13:17:45 +0200

    Changed to global rank on user card

--- src/Tabs/GeneralCards/GeneralCardUser.js
+++ src/Tabs/GeneralCards/GeneralCardUser.js
@@ -9,7 +9,7 @@ function GeneralCardUser(props) {
                     <Grid container spacing={3} sx={{ justifyContent: 'space-between' }}>
                         <Grid item>
                             <Typography color="textPrimary" variant="h4">{props.data.user.username}</Typography>
-                            <Typography color="textSecondary">{toFixedNumber(props.data.user.statistics.pp, 0).toLocaleString('en-US')}pp #{props.data.user.statistics.country_rank.toLocaleString('en-US')}</Typography>
+                            <Typography color="textSecondary">{toFixedNumber(props.data.user.statistics.pp, 0).toLocaleString('en-US')}pp #{props.data.user.statistics.global_rank.toLocaleString('en-US')}</Typography>
                         </Grid>
                         <Grid item>
                             <Avatar src={props.data.user.avatar_url} sx={{ width: 56, height: 56 }} />
