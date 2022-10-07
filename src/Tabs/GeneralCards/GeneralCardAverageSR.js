6aec8a8046f37d1d97f362acff9db9c134d72064
Amayakase <engineermarkpro@gmail.com>
2022-10-07 12:20:38 +0200

    Added asterisk to the sr cards

--- src/Tabs/GeneralCards/GeneralCardAverageSR.js
+++ src/Tabs/GeneralCards/GeneralCardAverageSR.js
@@ -8,7 +8,7 @@ function GeneralCardAverageSR(props) {
                 <CardContent>
                     <Grid container spacing={3} sx={{ justifyContent: 'space-between' }}>
                         <Grid item>
-                            <Typography color="textPrimary" variant="h4">{toFixedNumber(props.data.processed.average_sr, 2)}</Typography>
+                            <Typography color="textPrimary" variant="h4">{toFixedNumber(props.data.processed.average_sr, 2)}*</Typography>
                             <Typography variant="caption" color="textSecondary">average stars</Typography>
                         </Grid>
                     </Grid>
--- src/Tabs/GeneralCards/GeneralCardHighestSR.js
+++ src/Tabs/GeneralCards/GeneralCardHighestSR.js
@@ -8,7 +8,7 @@ function GeneralCardHighestSR(props) {
                 <CardContent>
                     <Grid container spacing={3} sx={{ justifyContent: 'space-between' }}>
                         <Grid item>
-                            <Typography color="textPrimary" variant="h4">{toFixedNumber(props.data.processed.highest_sr, 2)}</Typography>
+                            <Typography color="textPrimary" variant="h4">{toFixedNumber(props.data.processed.highest_sr, 2)}*</Typography>
                             <Typography color="textSecondary">highest star rating pass</Typography>
                         </Grid>
                     </Grid>
