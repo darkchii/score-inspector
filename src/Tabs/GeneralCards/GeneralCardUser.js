6130cba91becfc6a7837ecbe8c0278783525cc69
Mark Meijer <engineermarkpro@gmail.com>
2022-11-05 16:45:06 +0100

    Added best rank tooltip to user card

--- src/Tabs/GeneralCards/GeneralCardUser.js
+++ src/Tabs/GeneralCards/GeneralCardUser.js
@@ -1,4 +1,5 @@
-import { Avatar, Card, CardContent, Typography, Grid } from '@mui/material';
+import { Avatar, Card, CardContent, Typography, Grid, Tooltip } from '@mui/material';
+import moment from 'moment';
 import { toFixedNumber } from '../../helper';
 
 function GeneralCardUser(props) {
@@ -9,7 +10,9 @@ function GeneralCardUser(props) {
                     <Grid container spacing={3} sx={{ justifyContent: 'space-between' }}>
                         <Grid item>
                             <Typography color="textPrimary" variant="h4">{props.data.user.username}</Typography>
-                            <Typography color="textSecondary">{toFixedNumber(props.data.user.statistics.pp, 0).toLocaleString('en-US')}pp #{props.data.user.statistics.global_rank.toLocaleString('en-US')}</Typography>
+                            <Tooltip title={`Highest rank: #${props.data.user.rank_highest.rank}, ${moment(props.data.user.rank_highest.updated_at).fromNow()}`}>
+                                <Typography color="textSecondary">{toFixedNumber(props.data.user.statistics.pp, 0).toLocaleString('en-US')}pp #{props.data.user.statistics.global_rank.toLocaleString('en-US')}</Typography>
+                            </Tooltip>
                         </Grid>
                         <Grid item>
                             <Avatar src={props.data.user.avatar_url} sx={{ width: 56, height: 56 }} />
