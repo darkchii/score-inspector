b88107266c4c55204a916c60bb8fd316615a62ed
Mark Meijer <engineermarkpro@gmail.com>
2023-01-12 18:47:31 +0100

    Less tall tabs on userpage

---
+++ src/Components/UserPage/SectionComments.js
@@ -0,0 +1,9 @@
+function SectionComments(props) {
+    return (
+        <>
+            
+        </>
+    );
+}
+
+export default SectionComments;
--- src/Components/UserPage/UserDataContainer.js
+++ src/Components/UserPage/UserDataContainer.js
@@ -5,11 +5,19 @@ import PersonIcon from '@mui/icons-material/Person';
 import TodayIcon from '@mui/icons-material/Today';
 import AutoGraphIcon from '@mui/icons-material/AutoGraph';
 import FormatListBulletedIcon from '@mui/icons-material/FormatListBulleted';
+import InsertCommentIcon from '@mui/icons-material/InsertComment';
 import SectionCards from "./SectionCards";
 import SectionGrades from "./SectionGrades";
 import SectionGraphs from "./SectionGraphs";
 import SectionScores from "./SectionScores";
 import SectionDaily from "./SectionDaily";
+import SectionComments from "./SectionComments";
+import styled from "@emotion/styled";
+
+const StyledTab = styled(Tab)({
+    minHeight: 'auto',
+    padding: 10,
+});
 
 function UserDataContainer(props) {
     const [tab, setTab] = useState('0');
@@ -18,7 +26,8 @@ function UserDataContainer(props) {
         'profile': '0',
         'daily': '1',
         'graphs': '2',
-        'scores': '3'
+        'scores': '3',
+        'comments': '4'
     }
 
     return (
@@ -26,10 +35,11 @@ function UserDataContainer(props) {
             <TabContext value={tab}>
                 <Box component={Paper}>
                     <TabList onChange={(e, v) => setTab(v)} centered>
-                        <Tab icon={<PersonIcon />} iconPosition='start' label='Profile' value={_IDs['profile']} />
-                        <Tab icon={<TodayIcon />} iconPosition='start' label='Daily' value={_IDs['daily']} />
-                        <Tab icon={<AutoGraphIcon />} iconPosition='start' label='Graphs' value={_IDs['graphs']} />
-                        <Tab icon={<FormatListBulletedIcon />} iconPosition='start' label='Scores' value={_IDs['scores']} />
+                        <StyledTab icon={<PersonIcon />} iconPosition='start' label='Profile' value={_IDs['profile']} />
+                        <StyledTab icon={<TodayIcon />} iconPosition='start' label='Daily' value={_IDs['daily']} />
+                        <StyledTab icon={<AutoGraphIcon />} iconPosition='start' label='Graphs' value={_IDs['graphs']} />
+                        <StyledTab icon={<FormatListBulletedIcon />} iconPosition='start' label='Scores' value={_IDs['scores']} />
+                        <StyledTab icon={<InsertCommentIcon />} iconPosition='start' label='Comments' value={_IDs['comments']} />
                     </TabList>
                 </Box>
                 <TabPanel sx={{ p: 0 }} value={_IDs['profile']}>
@@ -46,6 +56,9 @@ function UserDataContainer(props) {
                 </TabPanel>
                 <TabPanel sx={{ p: 0 }} value={_IDs['scores']}>
                     <SectionScores user={props.user} />
+                </TabPanel>
+                <TabPanel sx={{ p: 0 }} value={_IDs['comments']}>
+                    <SectionComments user={props.user} />
                 </TabPanel>
             </TabContext>
         </>
