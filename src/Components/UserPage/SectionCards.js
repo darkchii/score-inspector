c140184aebfe5603c5c7b19263be3dfe3c86a56c
Mark Meijer <engineermarkpro@gmail.com>
2023-04-30 14:19:33 +0200

    Misc stat card redesigned, takes up less space

--- src/Components/UserPage/SectionCards.js
+++ src/Components/UserPage/SectionCards.js
@@ -1,4 +1,4 @@
-import { Grid, Paper, Typography, useTheme } from "@mui/material";
+import { Box, Grid, Paper, Typography, useTheme } from "@mui/material";
 import moment from "moment";
 import BestScoreCard from "./BestScoreCard";
 import Performance2016 from "./Performance/Performance2016";
@@ -22,87 +22,87 @@ function SectionCards(props) {
         {
             title: 'Clears',
             value: props.user.scores?.length.toLocaleString('en-US') ?? 0,
-            size: 1.25
+            size: 1
         },
         {
             title: 'Play Count',
             value: props.user.osu.statistics.play_count.toLocaleString('en-US'),
-            size: 1.25
+            size: 1
         },
         {
             title: 'Avg PP',
             value: (Math.round((props.user.data?.average.pp ?? 0) * 100) / 100).toLocaleString('en-US'),
-            size: 1.25
+            size: 1
         },
         {
             title: 'Avg Acc',
             value: (Math.round((props.user.data?.average.acc ?? 0) * 100) / 100).toLocaleString('en-US') + '%',
-            size: 1.25
+            size: 1
         },
         {
             title: 'Avg Length',
             value: moment.duration(props.user.data?.average.length ?? 0, 'seconds').format('mm:ss'),
-            size: 1.5
+            size: 1
         },
         {
             title: 'Avg Stars',
             value: (Math.round((props.user.data?.average.star_rating ?? 0) * 100) / 100).toLocaleString('en-US') + '*',
-            size: 1.25
+            size: 1
         },
         {
             title: 'Score per clear',
             value: Math.round(props.user.data?.average.score)?.toLocaleString('en-US') ?? 0,
-            size: 2.5
+            size: 1
         },
         {
             title: 'Fullcombo\'d',
             value: (((Math.round(props.user.data?.fcRate * 1000) ?? 0) / 10).toLocaleString('en-US') ?? 0) + '%',
-            size: 1.75
+            size: 1
         },
         {
             title: 'Ranked Score',
             value: props.user.data.total.score.toLocaleString('en-US'),
-            size: 2
+            size: 1.25
         },
         {
             title: 'Total Score',
             value: props.user.osu.statistics.total_score?.toLocaleString('en-US') ?? 'N/A',
-            size: 2
+            size: 1.25
         },
         {
             title: 'Lazer Score',
             value: Math.round(props.user.data?.total.scoreLazer)?.toLocaleString('en-US') ?? 0,
-            size: 2
+            size: 1.5
         },
         {
             title: 'Score Length',
             value: Math.round(moment.duration(props.user.data.total.length, 'seconds').asHours()) + ' hours',
-            size: 2
+            size: 1
         },
         {
             title: 'Profile playtime',
             value: Math.round(moment.duration(props.user.osu?.statistics.play_time ?? 0, 'seconds').asHours()) + ' hours',
-            size: 2
+            size: 1
         },
         {
             title: 'Approx playtime',
             value: Math.round(moment.duration(props.user.data.approximatePlaytime ?? 0, 'seconds').asHours()) + ' hours',
-            size: 2
+            size: 1
         },
         {
             title: 'Total sessions',
             value: (props.user.data.sessions.length ?? 0).toLocaleString('en-US'),
-            size: 1.5
+            size: 1
         },
         {
             title: 'Avg session length',
             value: moment.duration(props.user.data.sessions.length > 0 ? (props.user.data.totalSessionLength / props.user.data.sessions.length) : 0, 'seconds').format('h [hrs], m [min]'),
-            size: 2
+            size: 1.2
         },
         {
             title: 'Completion',
             value: (props.user.scores?.length > 0 ? Math.round((100 / props.user.data.total_beatmaps * props.user.scores?.length) * 100) / 100 : 0) + '%',
-            size: 1.5
+            size: 1
         },
         {
             title: 'Medals',
@@ -112,50 +112,51 @@ function SectionCards(props) {
         {
             title: 'Highest Combo',
             value: (props.user.alt.maximum_combo ?? 0).toLocaleString('en-US') + 'x',
-            size: 1.5
+            size: 1.2
         },
         {
             title: 'Unique SS',
             value: (props.user.alt.unique_ss?.length ?? 0).toLocaleString('en-US'),
-            size: 1.5
+            size: 1.2
         },
         {
             title: 'Unique FC',
             value: (props.user.alt.unique_fc?.length ?? 0).toLocaleString('en-US'),
-            size: 1.5
+            size: 1.2
         },
         {
             title: 'Unique DT FC',
             value: (props.user.alt.unique_dt_fc?.length ?? 0).toLocaleString('en-US'),
-            size: 1.5
+            size: 1.2
         }
     ];
 
     return (
         <>
-            <Grid container>
-                {
-                    _cards.map((card) => {
-                        return (
-                            <>
-                                <Grid item xs={card.size} sx={{ p: 0.2 }}>
-                                    <Paper elevation={2} sx={{ p: 1 }}>
-                                        <Grid container>
-                                            <Grid item xs={12} sx={{ textAlign: 'center' }}>
-                                                <Typography variant='title' sx={{ fontSize: '0.85em' }}>{card.title}</Typography>
+            <Paper elevation={2} sx={{ p: 1, pt: 2, pb: 2, pl: 2 }}>
+                <Grid container rowSpacing={2} columnSpacing={1}>
+                    {
+                        _cards.map((card) => {
+                            return (
+                                <>
+                                    <Grid item xs={card.size}>
+                                        <Grid container sx={{ p: 0.2, position: 'relative' }}>
+                                            <Box sx={{position: 'absolute', left:-8,borderRadius: 3, height: '100%', width: '4px', backgroundColor: theme.typography.title.color, boxShadow: `0px 0px 4px 2px ${theme.typography.title.color}22`}} />
+                                            <Grid item xs={12} sx={{ textAlign: 'left' }}>
+                                                <Typography variant='title' sx={{ fontSize: '0.75em' }}>{card.title}</Typography>
                                             </Grid>
-                                            <Grid item xs={12} sx={{ textAlign: 'center' }}>
-                                                <Typography variant='body1' sx={{ fontSize: '1.2em' }}>{card.value}</Typography>
+                                            <Grid item xs={12} sx={{ textAlign: 'left' }}>
+                                                <Typography variant='body1' sx={{ fontSize: '1.1em' }}>{card.value}</Typography>
                                             </Grid>
                                         </Grid>
-                                    </Paper>
-                                </Grid>
-                                {/* <Divider orientation='vertical' variant='middle' flexItem sx={{ mr: "-1px" }} /> */}
-                            </>
-                        )
-                    })
-                }
-            </Grid>
+                                    </Grid>
+                                    {/* <Divider orientation='vertical' variant='middle' flexItem sx={{ mr: "-1px" }} /> */}
+                                </>
+                            )
+                        })
+                    }
+                </Grid>
+            </Paper>
             <Grid container>
                 <Grid sx={{ minHeight: '100%', p: 0.5 }} item xs={3}><BestScoreCard valueTitle={'Top performance'} valueLabel={`${props.user.data.bestScores.best_pp.pp.toFixed(1)}pp`} score={props.user.data.bestScores.best_pp} /></Grid>
                 <Grid sx={{ minHeight: '100%', p: 0.5 }} item xs={3}><BestScoreCard valueTitle={'Top score'} valueLabel={`${props.user.data.bestScores.best_score.score.toLocaleString('en-US')} score`} score={props.user.data.bestScores.best_score} /></Grid>
@@ -163,12 +164,12 @@ function SectionCards(props) {
                 <Grid sx={{ minHeight: '100%', p: 0.5 }} item xs={3}><BestScoreCard valueTitle={'Oldest'} valueLabel={`${moment(props.user.data.bestScores.oldest.date_played).format('MMMM Mo YYYY')}`} score={props.user.data.bestScores.oldest} /></Grid>
             </Grid>
             <Grid container>
-                <Grid sx={{ minHeight: '100%', p: 0.5 }} item xs={12/6}><PerformanceFC data={props.user} /></Grid>
-                <Grid sx={{ minHeight: '100%', p: 0.5 }} item xs={12/6}><PerformanceSS data={props.user} /></Grid>
-                <Grid sx={{ minHeight: '100%', p: 0.5 }} item xs={12/6}><Performance2014 data={props.user} /></Grid>
-                <Grid sx={{ minHeight: '100%', p: 0.5 }} item xs={12/6}><Performance2016 data={props.user} /></Grid>
-                <Grid sx={{ minHeight: '100%', p: 0.5 }} item xs={12/6}><PerformanceLazer data={props.user} /></Grid>
-                <Grid sx={{ minHeight: '100%', p: 0.5 }} item xs={12/6}><PerformanceXexxar data={props.user} /></Grid>
+                <Grid sx={{ minHeight: '100%', p: 0.5 }} item xs={12 / 6}><PerformanceFC data={props.user} /></Grid>
+                <Grid sx={{ minHeight: '100%', p: 0.5 }} item xs={12 / 6}><PerformanceSS data={props.user} /></Grid>
+                <Grid sx={{ minHeight: '100%', p: 0.5 }} item xs={12 / 6}><Performance2014 data={props.user} /></Grid>
+                <Grid sx={{ minHeight: '100%', p: 0.5 }} item xs={12 / 6}><Performance2016 data={props.user} /></Grid>
+                <Grid sx={{ minHeight: '100%', p: 0.5 }} item xs={12 / 6}><PerformanceLazer data={props.user} /></Grid>
+                <Grid sx={{ minHeight: '100%', p: 0.5 }} item xs={12 / 6}><PerformanceXexxar data={props.user} /></Grid>
             </Grid>
             <Grid>
                 {
