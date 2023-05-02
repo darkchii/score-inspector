c7d89d02662f32c92dfe6ccf61006a28be808ff7
Mark Meijer <engineermarkpro@gmail.com>
2023-05-02 12:13:58 +0200

    Made playercard bit more dynamic

--- src/Components/PlayerCard.js
+++ src/Components/PlayerCard.js
@@ -4,6 +4,9 @@ import { Navigate } from "react-router-dom";
 import { GetFormattedName, GetRoleIcons } from "../Helpers/Account";
 import { PNG_LEVEL_BADGE } from "../Helpers/Assets";
 import LevelIcon from "./UI/LevelIcon";
+import { useState } from "react";
+import { useEffect } from "react";
+import { useRef } from "react";
 //expected user object:
 // {
 //     ...users2 user
@@ -12,12 +15,31 @@ import LevelIcon from "./UI/LevelIcon";
 //     }
 //}
 
-const CARD_HEIGHT = 150;
+const SECTIONAL_HEIGHT = 75;
 const BORDER_RADIUS = '15px';
 
 function PlayerCard(props) {
+    const [cardHeight, setCardHeight] = useState(0);
+    const [cardWidth, setCardWidth] = useState(0);
+    const boxRef = useRef();
+
+    const sx = {
+        rowHeight: SECTIONAL_HEIGHT,
+        ...props.sx,
+    }
+
+    useEffect(() => {
+        //get height of card element
+        setCardHeight(boxRef?.current?.clientHeight);
+        setCardWidth(boxRef?.current?.clientWidth);
+    });
+
     return (
-        <>
+        <Box
+            ref={boxRef}
+            sx={{
+                minHeight: '100%',
+            }}>
             <Card
                 onClick={() => props.onClick()}
                 elevation={5}
@@ -27,10 +49,12 @@ function PlayerCard(props) {
                         cursor: 'pointer'
                     },
                     position: 'relative',
-                    height: CARD_HEIGHT,
+                    height: '100%',
+                    minHeight: sx.rowHeight
                 }}>
                 <Grid sx={{
-                    height: CARD_HEIGHT * 0.5, display: 'flex',
+                    height: sx.rowHeight, display: 'flex',
+                    minHeight: sx.rowHeight,
                     backgroundImage: `url(${props.user.osu?.cover?.url})`,
                     backgroundSize: 'cover',
                     backgroundPosition: 'center',
@@ -47,7 +71,7 @@ function PlayerCard(props) {
                             component="img"
                             image={`https://a.ppy.sh/${props.user.user_id}`}
                             alt={props.user.username}
-                            sx={{ aspectRatio: '1/1', width: CARD_HEIGHT * 0.5, borderRadius: BORDER_RADIUS, display: 'flex' }} />
+                            sx={{ aspectRatio: '1/1', width: sx.rowHeight, borderRadius: BORDER_RADIUS, display: 'flex' }} />
                         <Box sx={{ display: 'flex', flexDirection: 'column', flex: '1 0 auto' }}>
                             <CardContent>
                                 <Box sx={{ textDecoration: 'none' }} alignContent='center'>
@@ -64,56 +88,62 @@ function PlayerCard(props) {
                                             />
                                             {props.user.username}
                                         </Typography>
-                                        <Stack direction='row' spacing={0.5}>
-                                            {props.user?.inspector_user?.roles && GetRoleIcons(props.user?.inspector_user?.roles, true)}
-                                        </Stack>
+                                        {
+                                            sx.rowHeight >= SECTIONAL_HEIGHT &&
+                                            <Stack direction='row' spacing={0.5}>
+                                                {props.user?.inspector_user?.roles && GetRoleIcons(props.user?.inspector_user?.roles, true)}
+                                            </Stack>
+                                        }
                                     </Stack>
                                 </Box>
                             </CardContent>
                         </Box>
                         <Box sx={{
                             display: 'flex',
-                            width: CARD_HEIGHT*0.5
+                            width: sx.rowHeight
                         }}>
                             <LevelIcon
-                                size={CARD_HEIGHT*0.5}
+                                size={sx.rowHeight}
                                 borderRadius={BORDER_RADIUS}
-                                level={props.user.osu.statistics_rulesets.osu.level.current} 
-                                levelProgress={props.user.osu.statistics_rulesets.osu.level.progress}/>
+                                level={props.user.osu.statistics_rulesets.osu.level.current}
+                                levelProgress={props.user.osu.statistics_rulesets.osu.level.progress} />
                         </Box>
                     </Box>
                 </Grid>
-                <Grid sx={{
-                    height: CARD_HEIGHT * 0.5, display: 'flex',
-                    width: '100%'
-                }}>
-                    <Box
-                        display="flex"
-                        justifyContent="center"
-                        alignItems="center"
-                        sx={{
-                            width: '100%',
-                            borderRadius: BORDER_RADIUS,
-                            p: 1,
-                        }}>
-                        <Grid container spacing={1}>
-                            <Grid item xs={6}>
-                                <Typography variant='subtitle1'>Global Rank</Typography>
-                                <Typography variant='h5' sx={{
-                                    background: "-webkit-linear-gradient(0deg, #8CF6FA, #F68DC4, #FEB887)",
-                                    WebkitBackgroundClip: 'text',
-                                    WebkitTextFillColor: 'transparent'
-                                }}>#{props.user.osu.statistics_rulesets.osu.global_rank > 0 ? (Math.max(0, props.user.osu.statistics_rulesets.osu.global_rank)).toLocaleString('en-US') : '-'}</Typography>
+                {
+                    cardHeight >= sx.rowHeight * 2 &&
+                    <Grid sx={{
+                        height: sx.rowHeight, display: 'flex',
+                        width: '100%'
+                    }}>
+                        <Box
+                            display="flex"
+                            justifyContent="center"
+                            alignItems="center"
+                            sx={{
+                                width: '100%',
+                                borderRadius: BORDER_RADIUS,
+                                p: 1,
+                            }}>
+                            <Grid container spacing={1}>
+                                <Grid item xs={6}>
+                                    <Typography variant='subtitle1'>Global Rank</Typography>
+                                    <Typography variant='h5' sx={{
+                                        background: "-webkit-linear-gradient(0deg, #8CF6FA, #F68DC4, #FEB887)",
+                                        WebkitBackgroundClip: 'text',
+                                        WebkitTextFillColor: 'transparent'
+                                    }}>#{props.user.osu.statistics_rulesets.osu.global_rank > 0 ? (Math.max(0, props.user.osu.statistics_rulesets.osu.global_rank)).toLocaleString('en-US') : '-'}</Typography>
+                                </Grid>
+                                <Grid item xs={6}>
+                                    <Typography variant='subtitle1'>Score Rank</Typography>
+                                    <Typography variant='h5'>#{props.user.score_rank !== undefined ? ((Math.max(0, props.user.score_rank?.rank)).toLocaleString('en-US')) : '10.000+'}</Typography>
+                                </Grid>
                             </Grid>
-                            <Grid item xs={6}>
-                                <Typography variant='subtitle1'>Score Rank</Typography>
-                                <Typography variant='h5'>#{props.user.score_rank !== undefined ? ((Math.max(0, props.user.score_rank?.rank)).toLocaleString('en-US')) : '10.000+'}</Typography>
-                            </Grid>
-                        </Grid>
-                    </Box>
-                </Grid>
+                        </Box>
+                    </Grid>
+                }
             </Card >
-        </>
+        </Box>
     )
 }
 
--- src/Routes/CompDev.js
+++ src/Routes/CompDev.js
@@ -1,73 +1,220 @@
 import { Grid, Paper } from "@mui/material";
 import LevelIcon from "../Components/UI/LevelIcon";
+import PlayerCard from "../Components/PlayerCard";
 
 function CompDev() {
+    const testUser = {
+        "user_id": 10153735,
+        "username": "boob enjoyer",
+        "country_code": "NL",
+        "global_rank": 140171,
+        "score_rank": {
+            "rank": 2844,
+            "user_id": 10153735,
+            "username": "boob enjoyer",
+            "score": 47855392433
+        },
+        "osu": {
+            "avatar_url": "https://a.ppy.sh/10153735?1682702595.png",
+            "country_code": "NL",
+            "default_group": "default",
+            "id": 10153735,
+            "is_active": true,
+            "is_bot": false,
+            "is_deleted": false,
+            "is_online": false,
+            "is_supporter": true,
+            "last_visit": null,
+            "pm_friends_only": false,
+            "profile_colour": null,
+            "username": "boob enjoyer",
+            "country": {
+                "code": "NL",
+                "name": "Netherlands"
+            },
+            "cover": {
+                "custom_url": null,
+                "url": "https://osu.ppy.sh/images/headers/profile-covers/c8.jpg",
+                "id": "8"
+            },
+            "groups": [],
+            "statistics_rulesets": {
+                "osu": {
+                    "count_100": 530436,
+                    "count_300": 8885441,
+                    "count_50": 46327,
+                    "count_miss": 151931,
+                    "level": {
+                        "current": 100,
+                        "progress": 51
+                    },
+                    "global_rank": 140171,
+                    "global_rank_exp": 25416,
+                    "pp": 3681.67,
+                    "pp_exp": 3681.68,
+                    "ranked_score": 47855392433,
+                    "hit_accuracy": 98.8736,
+                    "play_count": 70404,
+                    "play_time": 3918996,
+                    "total_score": 78355292622,
+                    "total_hits": 9462204,
+                    "maximum_combo": 5186,
+                    "replays_watched_by_others": 92,
+                    "is_ranked": true,
+                    "grade_counts": {
+                        "ss": 12424,
+                        "ssh": 1000,
+                        "s": 3346,
+                        "sh": 500,
+                        "a": 806
+                    }
+                },
+                "taiko": {
+                    "count_100": 731,
+                    "count_300": 3725,
+                    "count_50": 0,
+                    "count_miss": 280,
+                    "level": {
+                        "current": 7,
+                        "progress": 98
+                    },
+                    "global_rank": 86743,
+                    "global_rank_exp": null,
+                    "pp": 235.906,
+                    "pp_exp": 0,
+                    "ranked_score": 1354838,
+                    "hit_accuracy": 93.0067,
+                    "play_count": 75,
+                    "play_time": 2982,
+                    "total_score": 3067199,
+                    "total_hits": 4456,
+                    "maximum_combo": 133,
+                    "replays_watched_by_others": 0,
+                    "is_ranked": true,
+                    "grade_counts": {
+                        "ss": 0,
+                        "ssh": 0,
+                        "s": 6,
+                        "sh": 0,
+                        "a": 11
+                    }
+                },
+                "fruits": {
+                    "count_100": 15174,
+                    "count_300": 263375,
+                    "count_50": 256860,
+                    "count_miss": 9693,
+                    "level": {
+                        "current": 54,
+                        "progress": 90
+                    },
+                    "global_rank": 22789,
+                    "global_rank_exp": null,
+                    "pp": 951.022,
+                    "pp_exp": 0,
+                    "ranked_score": 675677539,
+                    "hit_accuracy": 99.6765,
+                    "play_count": 1540,
+                    "play_time": 72496,
+                    "total_score": 1088324105,
+                    "total_hits": 535409,
+                    "maximum_combo": 1201,
+                    "replays_watched_by_others": 1,
+                    "is_ranked": true,
+                    "grade_counts": {
+                        "ss": 65,
+                        "ssh": 0,
+                        "s": 258,
+                        "sh": 0,
+                        "a": 85
+                    }
+                },
+                "mania": {
+                    "count_100": 9369,
+                    "count_300": 47956,
+                    "count_50": 1232,
+                    "count_miss": 1333,
+                    "level": {
+                        "current": 32,
+                        "progress": 76
+                    },
+                    "global_rank": 342506,
+                    "global_rank_exp": null,
+                    "pp": 220.823,
+                    "pp_exp": 0,
+                    "ranked_score": 134327463,
+                    "hit_accuracy": 97.7785,
+                    "play_count": 496,
+                    "play_time": 31409,
+                    "total_score": 229177455,
+                    "total_hits": 58557,
+                    "maximum_combo": 1659,
+                    "replays_watched_by_others": 0,
+                    "is_ranked": true,
+                    "grade_counts": {
+                        "ss": 2,
+                        "ssh": 0,
+                        "s": 98,
+                        "sh": 0,
+                        "a": 32
+                    }
+                }
+            }
+        },
+        "inspector_user": {
+            "id": 1,
+            "osu_id": 10153735,
+            "known_username": "boob enjoyer",
+            "background_image": "https://cdn.donmai.us/original/ab/dd/__kousaka_kirino_ore_no_imouto_ga_konna_ni_kawaii_wake_ga_nai_drawn_by_ootomo_takuji__abdde8f03fcb63fb13a0cc36aa472cfa.png",
+            "roles": "[\"dev\", \"trusted\",\"vip\"]"
+        }
+    };
     return (
         <>
             <h2>Are you supposed to be here?</h2>
-            <Grid container sx={{ height: '50px' }} spacing={2}>
-                <Grid item xs={2} sx={{ height: '100%' }}>
-                    <Paper sx={{ height: '100%' }} elevation={5}>
-                        <LevelIcon />
-                    </Paper>
-                </Grid>
-                <Grid item xs={4} sx={{ height: '100%' }}>
-                    <Paper sx={{ height: '100%' }} elevation={5}>
-                        <LevelIcon />
-                    </Paper>
-                </Grid>
-                <Grid item xs={5} sx={{ height: '100%' }}>
-                    <Paper sx={{ height: '100%' }} elevation={5}>
-                        <LevelIcon />
-                    </Paper>
-                </Grid>
-                <Grid item xs={1} sx={{ height: '100%' }}>
-                    <Paper sx={{ height: '100%' }} elevation={5}>
-                        <LevelIcon />
-                    </Paper>
-                </Grid>
-            </Grid>
             <Grid container sx={{ height: '150px', pt: 1 }} spacing={2}>
                 <Grid item xs={2} sx={{ height: '100%' }}>
                     <Paper sx={{ height: '100%' }} elevation={5}>
-                        <LevelIcon />
+                        <PlayerCard user={testUser} sx={{
+                            rowHeight: 60
+                        }} />
                     </Paper>
                 </Grid>
                 <Grid item xs={4} sx={{ height: '100%' }}>
                     <Paper sx={{ height: '100%' }} elevation={5}>
-                        <LevelIcon />
+                        <PlayerCard user={testUser} />
                     </Paper>
                 </Grid>
                 <Grid item xs={5} sx={{ height: '100%' }}>
                     <Paper sx={{ height: '100%' }} elevation={5}>
-                        <LevelIcon />
+                        <PlayerCard user={testUser} />
                     </Paper>
                 </Grid>
                 <Grid item xs={1} sx={{ height: '100%' }}>
                     <Paper sx={{ height: '100%' }} elevation={5}>
-                        <LevelIcon />
+                        <PlayerCard user={testUser} />
                     </Paper>
                 </Grid>
             </Grid>
             <Grid container sx={{ height: '350px', pt: 1 }} spacing={2}>
                 <Grid item xs={2} sx={{ height: '100%' }}>
                     <Paper sx={{ height: '100%' }} elevation={5}>
-                        <LevelIcon />
+                        <PlayerCard user={testUser} />
                     </Paper>
                 </Grid>
                 <Grid item xs={4} sx={{ height: '100%' }}>
                     <Paper sx={{ height: '100%' }} elevation={5}>
-                        <LevelIcon />
+                        <PlayerCard user={testUser} />
                     </Paper>
                 </Grid>
                 <Grid item xs={5} sx={{ height: '100%' }}>
                     <Paper sx={{ height: '100%' }} elevation={5}>
-                        <LevelIcon />
+                        <PlayerCard user={testUser} />
                     </Paper>
                 </Grid>
                 <Grid item xs={1} sx={{ height: '100%' }}>
                     <Paper sx={{ height: '100%' }} elevation={5}>
-                        <LevelIcon />
+                        <PlayerCard user={testUser} />
                     </Paper>
                 </Grid>
             </Grid>
