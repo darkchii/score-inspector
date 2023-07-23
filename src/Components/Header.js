9fff5c3259ddf19d778203fc2b2f5b23c1d7818a
Mark Meijer <engineermarkpro@gmail.com>
2023-07-23 15:40:50 +0200

    header responsive to mobile resolutions

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,4 +1,4 @@
-import { AppBar, Avatar, Box, Button, Container, IconButton, Menu, Stack, Toolbar, Typography } from '@mui/material';
+import { AppBar, Avatar, Box, Button, Container, Divider, Drawer, IconButton, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Menu, Stack, Toolbar, Typography } from '@mui/material';
 import React from 'react';
 import { useRef } from 'react';
 import { Link } from 'react-router-dom';
@@ -16,14 +16,52 @@ import AccountDropdown from './Navigation/AccountDropdown';
 import CustomizeModal from './Modals/CustomizeModal';
 import VisitorLogModal from './Modals/VisitorLogModal';
 import MapIcon from '@mui/icons-material/Map';
+import MenuIcon from '@mui/icons-material/Menu';
 
 function Header(props) {
+    const { window } = props;
     const searchElement = useRef(null);
     const settingsElement = useRef(null);
     const customizeElement = useRef(null);
     const visitorLogElement = useRef(null);
+    const [mobileOpen, setMobileOpen] = React.useState(false);
 
     const [showMenu, setShowMenu] = React.useState(null);
+
+    const headerNavItems = [
+        { name: 'Search', icon: <SearchIcon />, onClick: () => searchElement.current.setOpen(true) },
+        { name: 'Leaderboards', icon: <LeaderboardIcon />, onClick: () => { }, linkTo: '/leaderboard' },
+        { name: 'Top Scores', icon: <StarIcon />, onClick: () => { }, linkTo: '/top' },
+        { name: 'Stats', icon: <DataUsageIcon />, onClick: () => { }, linkTo: '/stats' },
+        { name: 'Settings', icon: <SettingsIcon />, onClick: () => settingsElement.current.setOpen(true) },
+        { name: 'Tools', icon: <BuildIcon />, onClick: () => { }, linkTo: '/tools' },
+    ];
+
+    const handleDrawerToggle = () => {
+        setMobileOpen((prevState) => !prevState);
+    };
+
+    const drawerWidth = 240;
+    const navDropDownRenderer = (
+        <Box onClick={handleDrawerToggle} sx={{p:1}}>
+            <Typography variant="h6" sx={{ my: 2 }}>
+                osu! scores inspector
+            </Typography>
+            <Divider />
+            <List>
+                {headerNavItems.map((item) => (
+                    <ListItem key={item.name} disablePadding>
+                        <ListItemButton startIcon={item.icon}>
+                            <ListItemIcon sx={{ minWidth: '40px' }}>{item.icon}</ListItemIcon>
+                            <ListItemText primary={item.name} />
+                        </ListItemButton>
+                    </ListItem>
+                ))}
+            </List>
+        </Box>
+    )
+
+    const container = window !== undefined ? () => window().document.body : undefined;
 
     return (
         <>
@@ -39,21 +77,32 @@ function Header(props) {
                 <AppBar position="static">
                     <Container maxWidth='xl'>
                         <Toolbar disableGutters>
+                            <IconButton
+                                color="inherit"
+                                aria-label="open drawer"
+                                edge="start"
+                                onClick={handleDrawerToggle}
+                                sx={{ mr: 2, display: { lg: 'none' } }}
+                            >
+                                <MenuIcon />
+                            </IconButton>
                             <Typography variant='h6' noWrap component={Link} to={`/`} sx={{
                                 mr: 2,
                                 color: 'inherit',
                                 textDecoration: 'none',
                             }}>osu! scores inspector</Typography>
-                            <Box sx={{ flexGrow: 1 }}>
+                            <Box sx={{ display: { xs: 'none', sm: 'none', md: 'none', lg: 'block', flexGrow: 1 } }}>
                                 <Stack direction="row" spacing={2} sx={{ display: 'flex' }}>
-                                    <Button size='small' startIcon={<SearchIcon />} onClick={() => searchElement.current.setOpen(true)}>Search</Button>
-                                    {/* <Button size='small' startIcon={<QueryStatsIcon />} component={Link} to={'/stats'}>Stats</Button> */}
-                                    <Button size='small' startIcon={<LeaderboardIcon />} component={Link} to={'/leaderboard'}>Leaderboards</Button>
-                                    {/* <Button size='small' startIcon={<SportsEsportsIcon />} component={Link} to={'/beatmaps'}>Beatmaps</Button> */}
-                                    <Button size='small' startIcon={<StarIcon />} component={Link} to={'/top'}>Top Scores</Button>
-                                    <Button size='small' startIcon={<DataUsageIcon />} component={Link} to={'/stats'}>Stats</Button>
-                                    <Button size='small' startIcon={<SettingsIcon />} onClick={() => settingsElement.current.setOpen(true)}>Settings</Button>
-                                    <Button size='small' startIcon={<BuildIcon />} component={Link} to={'/tools'}>Tools</Button>
+                                    {
+                                        headerNavItems.map((item, index) => {
+                                            if (item.linkTo) {
+                                                return <Button key={index} size='small' startIcon={item.icon} component={Link} to={item.linkTo}>{item.name}</Button>
+                                            } else {
+                                                return <Button key={index} size='small' startIcon={item.icon} onClick={item.onClick}>{item.name}</Button>
+                                            }
+                                        }
+                                        )
+                                    }
                                 </Stack>
                             </Box>
                             <Box sx={{ flexGrow: 0 }}>
@@ -79,10 +128,10 @@ function Header(props) {
                                     <Box sx={{ width: '25em' }}>
                                         {
                                             props.account !== null ? <>
-                                                <AccountDropdown 
+                                                <AccountDropdown
                                                     visitorLogModal={visitorLogElement}
-                                                    customizeModal={customizeElement} 
-                                                    onClose={() => setShowMenu(null)} 
+                                                    customizeModal={customizeElement}
+                                                    onClose={() => setShowMenu(null)}
                                                     account={props.account} />
                                             </> : <>
                                                 <Login />
@@ -94,6 +143,22 @@ function Header(props) {
                         </Toolbar>
                     </Container>
                 </AppBar>
+                <Box component="nav">
+                    <Drawer
+                        container={container}
+                        variant="temporary"
+                        open={mobileOpen}
+                        onClose={handleDrawerToggle}
+                        ModalProps={{
+                            keepMounted: true, // Better open performance on mobile.
+                        }}
+                        sx={{
+                            '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+                        }}
+                    >
+                        {navDropDownRenderer}
+                    </Drawer>
+                </Box>
             </Box>
         </>
     );
