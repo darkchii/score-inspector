e81414dd8b8eaf552a82866ccfb509e845e996f5
Mark Meijer <engineermarkpro@gmail.com>
2023-08-28 17:33:55 +0200

    changed score submissions to mui chart system

--- src/Components/ScoreSubmissions.js
+++ src/Components/ScoreSubmissions.js
@@ -1,91 +1,19 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import { useEffect, useRef, useState } from "react";
-import { Button, ButtonGroup, Grid } from "@mui/material";
-import { Line } from "react-chartjs-2";
-import ChartDataLabels from 'chartjs-plugin-datalabels';
-import {
-    Chart as ChartJS,
-    CategoryScale,
-    LinearScale,
-    PointElement,
-    LineElement,
-    Title,
-    Tooltip,
-    Legend,
-    Filler
-} from 'chart.js';
-import Zoom from "chartjs-plugin-zoom";
+import { useEffect, useState } from "react";
+import { Button, ButtonGroup, Grid, useTheme } from "@mui/material";
 import { getScoreActivity } from "../Helpers/OsuAlt";
 import moment from "moment";
 import Loader from "./UI/Loader";
-ChartJS.register(
-    CategoryScale,
-    LinearScale,
-    PointElement,
-    LineElement,
-    Title,
-    Tooltip,
-    Legend,
-    ChartDataLabels,
-    Filler,
-    Zoom
-);
+import { LineChart } from "@mui/x-charts";
 
 function ScoreSubmissions(props) {
+    const theme = useTheme();
     const [hours, setHours] = useState(72);
     const [isWorking, setIsWorking] = useState(false);
 
     const [data, setData] = useState({
         labels: [],
         datasets: []
-    })
-
-    const [options, setOptions] = useState({
-        responsive: true,
-        plugins: {
-            legend: {
-                position: 'top',
-                display: false
-            },
-            title: {
-                display: false,
-                text: 'Chart.js Line Chart',
-            },
-            datalabels: {
-                display: 'auto',
-                color: 'white',
-                font: {
-                    family: "Roboto"
-                },
-                margin: {
-                    top: 15
-                },
-                anchor: 'end',
-                align: 'end',
-                backgroundColor: '#00000088',
-                borderRadius: 5,
-                formatter: props.formatter ?? ((value, context) => {
-                    return `${value.toLocaleString("en-US")}`;
-                })
-            }
-        },
-        maintainAspectRatio: false,
-        animation: true,
-        spanGaps: true,
-        layout: {
-            padding: {
-                left: 0,
-                right: 40,
-                top: 40,
-                bottom: 0
-            }
-        },
-        scales: {
-            y:
-            {
-                reverse: false,
-            }
-        }
     })
 
     useEffect(() => {
@@ -98,66 +26,26 @@ function ScoreSubmissions(props) {
                 return;
             }
 
-            var newData = {
-                labels: [],
-                datasets: []
-            }
-
-            setData(newData);
-
-            var countData = [];
+            var newData = [];
 
             for (let i = 0; i < data.hour_entries.length; i++) {
-                // let timestamp = `${moment.duration(moment.utc().startOf('hour').diff(moment(data.hour_entries[i].timestamp).startOf('hour'))).asHours()} hours ago`;
-                //use shortest datetime format
-                let timestamp = moment.utc(data.hour_entries[i].timestamp).local().format("MMM Do, hA");
+                let timestamp = moment.utc(data.hour_entries[i].timestamp).local();
                 let count = data.hour_entries[i].entry_count;
 
-                newData.labels.push(timestamp);
-                countData.push(count);
-            }
+                let _data = {
+                    data: count,
+                    timestamp: timestamp.toDate(),
+                    label: timestamp.format("MMM Do, hA")
+                };
 
-            const color = {
-                r: 255, g: 102, b: 158
+                newData.push(_data);
             }
-            var dataObject = {
-                label: "Score submissions per hour",
-                data: countData,
-                borderColor: `rgb(${color.r}, ${color.g}, ${color.b})`,
-                fill: 'start',
-                backgroundColor: (context) => {
-                    const ctx = context.chart.ctx;
-                    const gradient = ctx.createLinearGradient(0, 0, 0, 500);
-                    gradient.addColorStop(0, `rgba(${color.r}, ${color.g}, ${color.b}, 0.7)`);
-                    gradient.addColorStop(1, `rgba(${color.r}, ${color.g}, ${color.b}, 0)`);
-                    return gradient;
-                },
-                tension: 0.3,
-                pointRadius: 4,
-                pointHoverRadius: 5,
-                borderWidth: 1
-            };
-            newData.datasets.push(dataObject);
 
             setData(newData);
-
-            const _options = JSON.parse(JSON.stringify(options));
-            if (props.formatter !== undefined) {
-                _options.plugins.datalabels.formatter = props.formatter;
-            } else {
-                _options.plugins.datalabels.formatter = (value, context) => {
-                    return `${value.toLocaleString("en-US")}`;
-                };
-            }
-            _options.scales.y.reverse = props.reverse;
-            setOptions(_options);
-
             setIsWorking(false);
             // eslint-disable-next-line react-hooks/exhaustive-deps
         })();
     }, [hours]);
-
-    var chart = useRef(null);
 
     return (
         <>
@@ -171,9 +59,14 @@ function ScoreSubmissions(props) {
                     <Button variant={hours === 720 ? 'contained' : 'outlined'} onClick={() => setHours(720)}>1 month</Button>
                 </ButtonGroup>
             </Grid>
-            <Grid sx={{ height: 280, position: "relative", pt: 2 }}>
+            <Grid sx={{ height: 280, position: "relative"}}>
                 {
-                    isWorking ? <Loader /> : <Line ref={chart} options={options} data={data} />
+                    isWorking || !data || !Array.isArray(data) || data.length <= 0 ?
+                        <Loader /> :
+                        <LineChart
+                            series={[{ type: 'line', data: data.map((d) => d.data), color: `${theme.palette.primary.main}` }]}
+                            xAxis={[{ scaleType: 'time', data: data.map((d) => d.timestamp)}]}
+                        />
                 }
             </Grid>
         </>
