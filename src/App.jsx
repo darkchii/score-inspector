5d8e486a5845ded59520af58cc4d90c7cffb7d90
amayakase <engineermarkpro@gmail.com>
2026-05-25 17:56:18 +0200

    added death message :')

--- src/App.jsx
+++ src/App.jsx
@@ -258,6 +258,28 @@ function App() {
                             </Box>
                           </Typography>
                         </Alert>
+                        <br />
+                        <Typography variant="body1">osu! scores inspector has been temporarily shut down in favor of the upcoming 3.0 rewrite, and the following:</Typography>
+                        <Typography variant="body1">The IP address that hosts this server, and is my personal IP, has been banned by peppy at the moment due to causing extreme loads on the osu! web server.</Typography>
+                        <Typography variant="body1">There is no clear date for when we will be back up with the complete rewrite, as I cannot work on it right now and am not willing to ban evade to access osu! api.</Typography>
+                        <br /><Typography variant="body1">Join the osu!alt Discord for updates on this situation (and project).</Typography>
+                        <br /><br />
+                        <Typography variant="body1">
+                          The reason for the ban is likely my teams fetcher (I just want to be clear on this), which does NOT use the API, but scrapes the website.<br />
+                          <a href="https://kirino.sh/teams/" target="_blank" rel="noreferrer">https://kirino.sh/teams/</a><br />
+                          It worked as follows:<br />
+                          - Scrape the team leaderboards to find new teams (all modes)<br />
+                          - For each team, scrape the team page (only if not updated recently, say 24h) to get all members (THIS HAS AN API ENDPOINT NOW, but never got to implementing it)<br />
+                          - Each team update also gets the team flag image, to generate a team color based on it (this is likely the thing that caused the load)<br />
+                          - For each member, call the API to get all mode stats (50 per call)<br />
+                          - Generate team stats based on the member stats<br />
+                          - Rinse and repeat
+                        </Typography><br />
+                        <Typography variant="body1">
+                          This system has been running for a year or so, but AI scrapers likely caused peppy to crack down on it.<br />
+                          I have plans for a new system that will get rid of the scraping, but not sure how this will look yet.<br/><br/>
+                          Just putting this here so you are aware of what is up.
+                        </Typography>
                       </Box>
                     ) : basePage
                   )
